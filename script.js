0d3d864a6674da89b38294c30edc7c853685b6d2
Mayuri Mhavale <141899038+Mayurimh@users.noreply.github.com>
2025-03-19 14:54:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let input = document.querySelector('input');
+let textarea = document.querySelector('textarea');
+let submitLink = document.querySelector("#submitLink");
+let description;
+let subject;
+
+input.addEventListener("input",(e) => {
+    subject = e.target.value;
+});
+textarea.addEventListener("input", (e) =>{
+description=e.target.value;
+});
+
+submitLink.addEventListener("click",(e) => {
+    submitLink.href = 'mailto:mhavalemayuri@gmail.com? subject = ${subject}&body=${description}';
+});
